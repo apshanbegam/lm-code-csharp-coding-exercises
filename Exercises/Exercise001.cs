170198ea6fbc0650a6f6fd2f8e2afb808a1266f3
apshanbegam <94931797+apshanbegam@users.noreply.github.com>
2022-05-04 16:39:51 +0100

    Excersiee001 Function Fillup

--- Exercises/Exercise001.cs
+++ Exercises/Exercise001.cs
@@ -8,32 +8,58 @@ namespace Exercises
     {
         public string CapitalizeWord(string word)
         {
-            // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            switch (word)
+            {
+                case null: throw new ArgumentNullException(nameof(word));
+                case "": throw new ArgumentException("word cannot be empty");
+                default: return word[0].ToString().ToUpper() + word.Substring(1);
+            }
         }
 
         public string GenerateInitials(string firstName, string lastName)
         {
-            // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            if (firstName != "" && lastName != "")
+            {
+                return String.Format("{0}.{1}",
+                                  firstName[0], lastName[0]);
+            }
+            else
+            {
+                throw new ArgumentException("Name cannot be empty");
+            }
         }
 
         public double AddVat(double originalPrice, double vatRate)
         {
-            // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            return Math.Round(originalPrice + (originalPrice * vatRate / 100), 2);
         }
 
         public string Reverse(string sentence)
         {
-            // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+
+            switch (sentence)
+            {
+                case null: throw new ArgumentNullException(nameof(sentence));
+                case "": throw new ArgumentException("sentence cannot be empty");
+                default:
+                    {
+                        char[] chars = sentence.ToCharArray();
+                        Array.Reverse(chars);
+                        return new String(chars);
+                    }
+            }
         }
 
         public int CountLinuxUsers(List<User> users)
         {
-            // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            int counter = 0;
+            foreach (User user in users)
+            {
+                if(user.Type == "Linux") {
+                    counter++;
+                }
+            }
+            return counter;
         }
     }
 }
