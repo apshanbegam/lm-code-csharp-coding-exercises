651476bde240b3a64767270b8b7549120c44ddaf
apshanbegam <94931797+apshanbegam@users.noreply.github.com>
2022-05-05 03:12:11 +0100

    Exercise003 code completion

--- Exercises/Exercise003.cs
+++ Exercises/Exercise003.cs
@@ -3,10 +3,9 @@ namespace Exercises
 {
     public class Exercise003
     {
-        // Replace the exception statement below with your code!
-        public string[] IceCreamFlavours => throw new NotImplementedException();
-
-        // Replace the exception statement below with your code!
-        public int IceCreamCode(string iceCreamFlavour) => throw new NotImplementedException();
+        
+        public string[] IceCreamFlavours = { "Pistachio", "Raspberry Ripple", "Vanilla", "Mint Chocolate Chip", "Chocolate", "Mango Sorbet" };
+       
+        public int IceCreamCode(string iceCreamFlavour) => Array.FindIndex(IceCreamFlavours, flavour => flavour == iceCreamFlavour); 
     }
 }
